881d3d2739928b0976de654a0a80d1fffbd3f4e6
natsuwiki <onegame1997@126.com>
2026-02-26 18:08:24 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   ignoreDeadLinks: true,
-  base: '/docs-demo/',
+  base: '/',
   title: "Natsu Cow",
   description: "A VitePress Site",
   themeConfig: {
